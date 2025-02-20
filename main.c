ce5c95d24e0a8d38a29909a8b7ddb3030efaa338
dborian <edmedorian@gmail.com>
2025-02-20 23:07:18 +0100

    second commit

--- main.c
+++ main.c
@@ -1,31 +1,169 @@
-#include <mlx_linux/mlx.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dedme <dedme@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 22:37:51 by dedme             #+#    #+#             */
+/*   Updated: 2025/02/20 23:04:42 by dedme            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
-}				t_vars;
+#include "solong.h"
 
-int	resize()
+int	open_file(char *path_file)
 {
-	printf("windows resized\n");
-	return (0);
+	int	fd;
+
+	fd = open(path_file, O_RDONLY);
+	if (fd == -1)
+	{
+		write(1, "Error opening the file\n", 23);
+		return (0);
+	}
+	return (fd);
 }
 
-int	close(int keycode, t_vars *vars)
+int	get_file_len(char *path_file)
 {
-	if (keycode == 65307)
-		mlx_destroy_window(vars->mlx, vars->win);
-	return (0);
+	int		nb_read;
+	int		fd;
+	int		count;
+	char	buf [1];
+
+	nb_read = -1;
+	count = 0;
+	fd = open_file(path_file);
+	while (nb_read != 0)
+	{
+		nb_read = read(fd, buf, 1);
+		if (nb_read == -1)
+		{
+			write (2, "Error can't read the file\n", 26);
+			return (0);
+		}
+		count++;
+	}
+	close (fd);
+	return (count);
 }
 
-int	main(void)
+char	*read_file(char *path_file, char *buf, int file_len)
 {
-	t_vars	vars;
+	int	nb_read;
+	int	count;
+	int	fd;
 
-	vars.mlx = mlx_init();
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
-	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
-	mlx_hook(vars.win, 4, 1L<<18, resize, &vars);
-	mlx_loop(vars.mlx);
+	nb_read = -1;
+	count = 0;
+	fd = open_file (path_file);
+	if (nb_read != 0)
+	{
+		nb_read = read(fd, buf, file_len);
+		if (nb_read == -1)
+		{
+			write (2, "Error can't read the file\n", 26);
+			return (NULL);
+		}
+		buf[nb_read] = '\0';
+	}
+	close (fd);
+	return (buf);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	return (i);
+}
+
+int	ft_chain_calc(char *str, char sep)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if (str[i] == '\0')
+		return (0);
+	while (str[i])
+	{
+		if (str[i] == sep)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+char	*ft_strdup(char *str)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	dup = malloc(sizeof(char) * ft_strlen(str) + 1);
+	if (dup == NULL)
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	**ft_split(char *str, char sep)
+{
+	int		i;
+	int		j;
+	char	**tab;
+
+	i = 0;
+	j = 0;
+	tab = malloc(sizeof(tab) * ft_chain_calc(str, sep) + 1);
+	if (tab == NULL)
+		return (NULL);
+	tab[j++] = ft_strdup(str);
+	i++;
+	while (str[i])
+	{
+		if (str[i - 1] == sep)
+			tab[j++] = ft_strdup(&str[i]);
+		i++;
+	}
+	free(str);
+	return (tab);
+}
+
+int	*ft_spawnpoint(int *inf, char **tab)
+{
+	
+}
+
+int main(int argc, char **argv)
+{
+	char 	*buf;
+	int 	file_len;
+	char	**tab;
+	int		i;
+	int		inf[4];
+	
+	if (argc < 2)
+		return (0);
+	i = 0;
+	buf = NULL;
+	file_len = get_file_len(argv[1]);
+	buf = malloc(sizeof(char) * file_len + 1);
+	read_file(argv[1], buf, file_len - 1);
+	printf("buf = %s\n\n", buf);
+	tab = ft_split(buf, '\n');
+	ft_spawnpoint(&inf, tab);
+	
+	ft_verif();
 }
---
+++ test.c
@@ -0,0 +1,35 @@
+#include <mlx_linux/mlx.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int	resize()
+{
+	printf("windows resized\n");
+	return (0);
+}
+
+int	close(int keycode, t_vars *vars)
+{
+	if (keycode == 65307)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+	printf("%d\n",mlx_hook(vars.win, 2, 1L<<0, close, &vars));
+	mlx_hook(vars.win, 4, 1L<<18, resize, &vars);
+	mlx_loop(vars.mlx);
+}
